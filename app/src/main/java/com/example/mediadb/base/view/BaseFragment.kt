b87933ac04f8bb25d7c40cae3e3b7b8bc8e9ba7b
FRAMGIA\vu.duc.chinh <vu.duc.chinh@sun-asterick.com>
2019-06-26 16:08:33 +0700

    Create Movie Detail screen

--- app/src/main/java/com/example/mediadb/base/view/BaseFragment.kt
+++ app/src/main/java/com/example/mediadb/base/view/BaseFragment.kt
@@ -3,8 +3,9 @@ package com.example.mediadb.base.view
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.LifecycleOwner
 
-abstract class BaseFragment : Fragment() {
+abstract class BaseFragment : Fragment(), LifecycleOwner {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
