6c55700fc878acc90ccbece12c6765d72d419bb1
FRAMGIA\vu.duc.chinh <vu.duc.chinh@sun-asterick.com>
2019-06-27 14:05:13 +0700

    Implement Koin - Fix bug comment 1

--- app/src/main/java/com/example/mediadb/MainApplication.kt
+++ app/src/main/java/com/example/mediadb/MainApplication.kt
@@ -1,13 +1,12 @@
 package com.example.mediadb
 
 import android.app.Application
-import androidx.lifecycle.LifecycleObserver
 import com.example.mediadb.di.module.repositoryModule
 import com.example.mediadb.di.module.viewModelModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
-class MainApplication : Application(), LifecycleObserver {
+class MainApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
